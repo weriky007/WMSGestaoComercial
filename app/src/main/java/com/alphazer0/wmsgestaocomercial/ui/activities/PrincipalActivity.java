a3cb907091836f3701750aeae5834b13677425dc
Alphazer0 <weriky.alphazer0@gmail.com>
2022-03-27 19:49:57 -0300

    editando menu e icones

--- app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/PrincipalActivity.java
+++ app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/PrincipalActivity.java
@@ -3,9 +3,14 @@ package com.alphazer0.wmsgestaocomercial.ui.activities;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alphazer0.wmsgestaocomercial.R;
@@ -18,23 +23,26 @@ import com.alphazer0.wmsgestaocomercial.ui.activities.vendas.VendasActivity;
 
 public class PrincipalActivity extends AppCompatActivity {
 
+    public static final String WMS_GESTÃO_COMERCIAL = "WMS Gestão Comercial";
     private ImageButton btnFornecedores;
     private ImageButton btnClientes;
     private ImageButton btnVendas;
     private ImageButton btnEstoque;
     private ImageButton btnContas;
     private ImageButton btnRelatorios;
+
 //==================================================================================================
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        setTitle(WMS_GESTÃO_COMERCIAL);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
         bindElementos();
         configuraBotoes();
     }
 //==================================================================================================
-    private void bindElementos(){
+    private void bindElementos() {
         btnFornecedores = findViewById(R.id.btnPrincipalFornecedoresActivity);
         btnClientes = findViewById(R.id.btnPrincipalClientesActivity);
         btnVendas = findViewById(R.id.btnAdicionaItemNaListaActivity);
@@ -43,7 +51,32 @@ public class PrincipalActivity extends AppCompatActivity {
         btnRelatorios = findViewById(R.id.btnPrincipalRelatoriosActivity);
     }
 //==================================================================================================
-    private void configuraBotoes(){
+    //MENU APPBAR
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_activity_principal, menu);
+//        MenuItem config = menu.add(0,0,0,"Configurações");
+//        config.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+//        MenuItem manual = menu.add(0,1,1,"Manual");
+//        manual.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case 0:
+                Toast.makeText(this, ""+item.getItemId(), Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+                Toast.makeText(this, ""+item.getItemId(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+//==================================================================================================
+    private void configuraBotoes() {
         btnFornecedores.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
