5289fb1eb8cc13550024f5a33e5e4c316c197425
Alphazer0 <weriky.alphazer0@gmail.com>
2022-04-01 17:01:03 -0300

    trabalhando nas classes de contas a pagar e receber

--- app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/contas/ContasPrincipal.java
+++ app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/contas/ContasPrincipal.java
@@ -1,7 +1,10 @@
 package com.alphazer0.wmsgestaocomercial.ui.activities.contas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +14,34 @@ import com.alphazer0.wmsgestaocomercial.R;
 public class ContasPrincipal extends AppCompatActivity {
 
     public static final String CONTAS = "Contas";
-
+    private ImageButton btnContasPagar;
+    private ImageButton btnContasReceber;
+//==================================================================================================
     @Override
     protected  void onCreate(Bundle savedIntanceState){
         super.onCreate(savedIntanceState);
         setTitle(CONTAS);
         setContentView(R.layout.activity_contas_principal);
+        configuraBotoes();
     }
+//==================================================================================================
+    private void configuraBotoes(){
+        btnContasPagar = findViewById(R.id.btn_contas_a_pagar);
+        btnContasReceber = findViewById(R.id.btn_contas_a_receber);
+
+        btnContasPagar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ContasPrincipal.this,ContasAPagar.class));
+            }
+        });
+
+        btnContasReceber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ContasPrincipal.this, ContasAReceber.class));
+            }
+        });
+    }
+//==================================================================================================
 }
