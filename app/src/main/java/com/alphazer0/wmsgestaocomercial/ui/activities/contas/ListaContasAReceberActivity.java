8d342463a244efe52cd6af794ef946f932ac1821
Alphazer0 <weriky.alphazer0@gmail.com>
2022-04-04 18:00:02 -0300

    configurando a lista de compras a receber

--- app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/contas/ListaContasAReceberActivity.java
+++ app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/contas/ListaContasAReceberActivity.java
@@ -1,13 +1,21 @@
 package com.alphazer0.wmsgestaocomercial.ui.activities.contas;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.InsetDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,6 +37,9 @@ import java.util.List;
 public class ListaContasAReceberActivity extends AppCompatActivity {
 
     public static final String CONTAS_A_RECEBER = "Contas a Receber";
+    public static final String ADICIONA_CONTA_A_RECEBER = "Adiciona Conta a Receber";
+    public static final String CONCLUIR = "Concluir";
+    public static final String CANCELAR = "Cancelar";
     private TextView vlTotalContasAReceber;
     private FloatingActionButton fabAddContaAReceber;
     private ListView listaContasAReceber;
@@ -52,8 +63,7 @@ public class ListaContasAReceberActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contas_a_receber);
         telaEmModoRetrado();
 
-        vlTotalContasAReceber = findViewById(R.id.valor_contas_a_receber);
-
+        bind();
         configuraAdapter();
         configuraLista();
         configuraFabAddContaAReceber();
@@ -62,6 +72,8 @@ public class ListaContasAReceberActivity extends AppCompatActivity {
     private void telaEmModoRetrado() {
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
     }
+
+    private void bind(){vlTotalContasAReceber = findViewById(R.id.valor_contas_a_receber);}
 
     private void configuraAdapter() {
         adapter = new ListaContasAReceberAdapter(contasAReceber);
@@ -72,13 +84,75 @@ public class ListaContasAReceberActivity extends AppCompatActivity {
         listaContasAReceber = findViewById(R.id.list_view_lista_contas_a_receber);
         listaContasAReceber.setAdapter(adapter);
     }
-
+//==================================================================================================
     private void configuraFabAddContaAReceber() {
         fabAddContaAReceber = findViewById(R.id.fab_adiciona_conta_a_receber);
         fabAddContaAReceber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+              abreFormularioContaAReceber();
+            }
+        });
+    }
+//==================================================================================================
+    private void abreFormularioContaAReceber(){
+        View viewAddContaReceber = LayoutInflater.from(ListaContasAReceberActivity.this)
+                .inflate(R.layout.activity_formulario_adiciona_conta_a_receber,null);
 
+        bindElementos(viewAddContaReceber);
+        configuraScanner(viewAddContaReceber);
+
+        ColorDrawable back = new ColorDrawable(Color.WHITE);
+        InsetDrawable inset = new InsetDrawable(back, 0);
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setMessage(ADICIONA_CONTA_A_RECEBER);
+        alertDialog.setView(viewAddContaReceber);
+
+        //ADICIONANDO A CONTA A LISTA
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, CONCLUIR, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //ESTA SOBREESCRITO ABAIXO
+            }
+        });
+        alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, CANCELAR, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(context, "Cancelado!", Toast.LENGTH_SHORT).show();
+                alertDialog.dismiss();
+            }
+        });
+
+        alertDialog.show();
+        alertDialog.getWindow().setBackgroundDrawable(inset);
+        Button btn = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+
+        //SOBREESCREVENDO O CLICK DO BOTAO POSITIVO
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                realizaVerificacao(alertDialog);
+//                listaComprasDAO.removeTodos(listaCompras);
+//                listaComprasDAO.salva(listaCompras);
+            }
+        });
+    }
+
+
+    private void bindElementos(View viewAddContaReceber) {
+        campoCodBarras = viewAddContaReceber.findViewById(R.id.edit_d_conta_a_receber_codigo_barras);
+        campoConta = viewAddContaReceber.findViewById(R.id.edit_conta_a_receber);
+        campoValor = viewAddContaReceber.findViewById(R.id.edit_valor_conta_a_receber);
+        campoDataVencimento = viewAddContaReceber.findViewById(R.id.edit_data_conta_a_receber);
+    }
+
+    private void configuraScanner(View viewAddContaReceber) {
+        fabLerCodigo = viewAddContaReceber.findViewById(R.id.fab_scan_d_conta_receber);
+        fabLerCodigo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanCode.scanCode(activity);
             }
         });
     }
