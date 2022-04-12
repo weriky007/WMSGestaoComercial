181d0d5e33a5f71760e5848eaafdaed3f737f2b6
Alphazer0 <weriky.alphazer0@gmail.com>
2022-04-11 21:40:30 -0300

    Configurando os inputs do grafico de contas

--- app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/contas/ContasPrincipalActivity.java
+++ app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/contas/ContasPrincipalActivity.java
@@ -11,10 +11,19 @@ import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alphazer0.wmsgestaocomercial.R;
+import com.alphazer0.wmsgestaocomercial.database.TotalContasAPagarDatabase;
+import com.alphazer0.wmsgestaocomercial.database.TotalContasAReceberDatabase;
+import com.alphazer0.wmsgestaocomercial.database.roomDAO.RoomTotalContasAPagarDAO;
+import com.alphazer0.wmsgestaocomercial.database.roomDAO.RoomTotalContasAReceberDAO;
+import com.alphazer0.wmsgestaocomercial.model.TotalContasAPagar;
+import com.alphazer0.wmsgestaocomercial.model.TotalContasAReceber;
 import com.androidplot.pie.PieChart;
 import com.androidplot.pie.PieRenderer;
 import com.androidplot.pie.Segment;
 import com.androidplot.pie.SegmentFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContasPrincipalActivity extends AppCompatActivity {
 
@@ -22,7 +31,10 @@ public class ContasPrincipalActivity extends AppCompatActivity {
     private ImageButton btnContasPagar;
     private ImageButton btnContasReceber;
     private PieChart graficoPizza;
-//==================================================================================================
+    private RoomTotalContasAReceberDAO totalContasAReceberDAO;
+    private RoomTotalContasAPagarDAO totalContasAPagarDAO;
+
+    //==================================================================================================
     @Override
     protected void onCreate(Bundle savedIntanceState) {
         super.onCreate(savedIntanceState);
@@ -32,11 +44,13 @@ public class ContasPrincipalActivity extends AppCompatActivity {
         configuraBotoes();
         configurandoGraficoPizza();
     }
-//==================================================================================================
+
+    //==================================================================================================
     private void mantemAtelaEmModoRetrato() {
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
     }
-//==================================================================================================
+
+    //==================================================================================================
     private void configuraBotoes() {
         btnContasPagar = findViewById(R.id.btn_contas_a_pagar);
         btnContasReceber = findViewById(R.id.btn_contas_a_receber);
@@ -55,8 +69,9 @@ public class ContasPrincipalActivity extends AppCompatActivity {
             }
         });
     }
-//==================================================================================================
-    private void configurandoGraficoPizza(){
+
+    //==================================================================================================
+    private void configurandoGraficoPizza() {
         //INFLANDO A VIEW
         View viewGrafico = getLayoutInflater().inflate(R.layout.grafico_contas_principal_activity, null);
         LinearLayout linearLayout = findViewById(R.id.linear_grafic_contas);
@@ -65,23 +80,35 @@ public class ContasPrincipalActivity extends AppCompatActivity {
         //BIND DO ELEMENTO
         graficoPizza = viewGrafico.findViewById(R.id.grafico_contas);
 
-        //LEGENDA E DADOS QUE SERAO INSERIDOS NO GRAFICO
-        Segment segment_caixa = new Segment("Caixa",40);
-        Segment segment_pagar = new Segment("A Pagar",35);
-        Segment segment_receber = new Segment("A Receber",15);
+        totalContasAReceberDAO = TotalContasAReceberDatabase.getInstance(this).getTotalContasAReceberDAO();
+        totalContasAPagarDAO = TotalContasAPagarDatabase.getInstance(this).getTotalContasAPagarDAO();
 
-        //SETANDO AS CORES EM RGB
-        SegmentFormatter formatterCaixa = new SegmentFormatter(Color.rgb(0,0,205));
-        SegmentFormatter formatterPagar = new SegmentFormatter(Color.rgb(255,0,0));
-        SegmentFormatter formatterReceber = new SegmentFormatter(Color.rgb(50,205,50));
+        //GRAFICO A PAGAR
+//        double dPagar = Double.parseDouble(totalContasAPagarDAO.totalContasAPagar().getTotal());
+//        Segment segment_pagar = new Segment("A Pagar: R$" + dPagar, dPagar);
+//        SegmentFormatter formatterPagar = new SegmentFormatter(Color.rgb(255, 0, 0));
+//        graficoPizza.addSegment(segment_pagar, formatterPagar);
+//        formatterPagar.getLabelPaint().setTextSize(40f);
+//        formatterPagar.getLabelPaint().setColor(Color.WHITE);
 
-        //SETANDO AS CONFIGURACOES DE CORES NO GRAFICO
-        graficoPizza.addSegment(segment_caixa,formatterCaixa);
-        graficoPizza.addSegment(segment_pagar,formatterPagar);
-        graficoPizza.addSegment(segment_receber,formatterReceber);
+        //GRAFICO A RECEBER
+        double dReceber = Double.parseDouble(totalContasAReceberDAO.totalContasAReceber().getTotal());
+        Segment segment_receber = new Segment("A Receber: R$" + dReceber, dReceber);
+        SegmentFormatter formatterReceber = new SegmentFormatter(Color.rgb(50, 205, 50));
+        graficoPizza.addSegment(segment_receber, formatterReceber);
+        formatterReceber.getLabelPaint().setTextSize(40f);
+
+        //GRAFICO CAIXA
+        Segment segment_caixa = new Segment("Caixa", 40);
+        formatterReceber.getLabelPaint().setColor(Color.WHITE);
+        SegmentFormatter formatterCaixa = new SegmentFormatter(Color.rgb(0, 0, 205));
+        graficoPizza.addSegment(segment_caixa, formatterCaixa);
+        formatterCaixa.getLabelPaint().setTextSize(40f);//TAMANHO TEXTO LEGENDA
+        formatterCaixa.getLabelPaint().setColor(Color.WHITE);//COR DA LEGENDA
+        formatterCaixa.setOffset(30);//SEPARA FATIA
 
         //MUDANDO ONDE O GRAFICO SE INICIA
-//        graficoPizza.getRenderer(PieRenderer.class).setStartDegs(90);
+        //graficoPizza.getRenderer(PieRenderer.class).setStartDegs(90);
 
         //CASO QUEIRA CORTAR O CIRCULO
         //graficoPizza.getRenderer(PieRenderer.class).setExtentDegs(190);
@@ -89,20 +116,6 @@ public class ContasPrincipalActivity extends AppCompatActivity {
         //CASO QUEIRA MUDAR A LARGURA
         //graficoPizza.getRenderer(PieRenderer.class).setDonutSize(0.9f, PieRenderer.DonutMode.PERCENT);
         //graficoPizza.getRenderer(PieRenderer.class).setDonutSize(30, PieRenderer.DonutMode.PIXELS);
-
-        //CASO QUEIRA MUDAR O TAMANHO DAS LETRAS DAS LEGENDAS
-        formatterCaixa.getLabelPaint().setTextSize(40f);
-        formatterReceber.getLabelPaint().setTextSize(40f);
-        formatterPagar.getLabelPaint().setTextSize(40f);
-        //MUDAR A COR DA LEGENDA
-        formatterCaixa.getLabelPaint().setColor(Color.WHITE);
-        formatterReceber.getLabelPaint().setColor(Color.WHITE);
-        formatterPagar.getLabelPaint().setColor(Color.WHITE);
-
-        //CASO QUEIRA SEPARAR UMA FATIA
-        formatterCaixa.setOffset(30);
-//        formatterReceber.setOffset(30);
-//        formatterPagar.setOffset(30);
     }
 //==================================================================================================
 }
