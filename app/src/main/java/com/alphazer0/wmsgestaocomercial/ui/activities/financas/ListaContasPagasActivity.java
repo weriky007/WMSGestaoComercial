74d36694cee15a234c1ba186132915d51f179870
Alphazer0 <weriky.alphazer0@gmail.com>
2022-04-19 15:53:53 -0300

    Criando as listas de contas pagas e recebidas

---
+++ app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/financas/ListaContasPagasActivity.java
@@ -0,0 +1,6 @@
+package com.alphazer0.wmsgestaocomercial.ui.activities.financas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListaContasPagasActivity extends AppCompatActivity {
+}
