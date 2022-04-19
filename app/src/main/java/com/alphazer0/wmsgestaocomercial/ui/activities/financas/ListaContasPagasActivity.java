5e373ef8f957568ce91b5a22c2c8fd1a1acc84d9
Alphazer0 <weriky.alphazer0@gmail.com>
2022-04-19 16:09:28 -0300

    Criando as listas de contas pagas e recebidas

--- app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/financas/ListaContasPagasActivity.java
+++ app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/financas/ListaContasPagasActivity.java
@@ -1,6 +1,22 @@
 package com.alphazer0.wmsgestaocomercial.ui.activities.financas;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.alphazer0.wmsgestaocomercial.R;
 public class ListaContasPagasActivity extends AppCompatActivity {
+
+    public static final String CONTAS_PAGAS = "Contas Pagas";
+
+    @Override
+    protected void onCreate(Bundle savedIntanceState){
+        super.onCreate(savedIntanceState);
+        setContentView(R.layout.activity_lista_contas_pagas);
+        setTitle(CONTAS_PAGAS);
+        mantemAtelaEmModoRetrato();
+    }
+
+    private void mantemAtelaEmModoRetrato() {
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
+    }
 }
--- app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/financas/ListaContasRecebidasActivity.java
+++ app/src/main/java/com/alphazer0/wmsgestaocomercial/ui/activities/financas/ListaContasRecebidasActivity.java
@@ -1,6 +1,25 @@
 package com.alphazer0.wmsgestaocomercial.ui.activities.financas;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.alphazer0.wmsgestaocomercial.R;
+
 public class ListaContasRecebidasActivity extends AppCompatActivity {
+
+    public static final String CONTAS_RECEBIDAS = "Contas Recebidas";
+
+    @Override
+    protected void onCreate(Bundle savedIntanceState){
+        super.onCreate(savedIntanceState);
+        setContentView(R.layout.activity_lista_contas_recebidas);
+        setTitle(CONTAS_RECEBIDAS);
+        mantemAtelaEmModoRetrato();
+    }
+
+    private void mantemAtelaEmModoRetrato() {
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
+    }
 }
